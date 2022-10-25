23c43e98e084eeed61c6f7f657f3b88b8a7b0cb9
Carlos Gasull <carlos.gasull@gmail.com>
2022-10-25 18:44:14 +0200

    Add files via upload

---
+++ S05T01N01/src/main/java/cat/itacademy/barcelonactiva/gasull/carlos/s05/t01/n01/S05T01N01GasullCarlosApplication.java
@@ -0,0 +1,26 @@
+package cat.itacademy.barcelonactiva.gasull.carlos.s05.t01.n01;
+
+import cat.itacademy.barcelonactiva.gasull.carlos.s05.t01.n01.DTO.SucursalDTO;
+import cat.itacademy.barcelonactiva.gasull.carlos.s05.t01.n01.model.Sucursal;
+import cat.itacademy.barcelonactiva.gasull.carlos.s05.t01.n01.repository.SucursalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S05T01N01GasullCarlosApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S05T01N01GasullCarlosApplication.class, args);
+	}
+
+	@Autowired
+	private SucursalRepository repositorio;
+
+	@Override
+	public void run(String... args) throws Exception {
+		/*Sucursal sucursal1 = new Sucursal("Alfajores Antonio","Argentina","fueraUE");
+		repositorio.save(sucursal1);*/
+	}
+}
---
+++ S05T01N01/src/test/java/cat/itacademy/barcelonactiva/gasull/carlos/s05/t01/n01/S05T01N01GasullCarlos/S05T01N01GasullCarlosApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.gasull.carlos.s05.t01.n01.S05T01N01GasullCarlos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S05T01N01GasullCarlosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
